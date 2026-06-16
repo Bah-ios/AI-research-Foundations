3f0b8dfc71509d482b9c3554d516a7e8162b39e6
Bahran Solomon tadesse <bahran.solomon@astu.edu.et>
2026-06-16 14:51:59 +0300

    import and setup

---
+++ 01 - Building my own Small language model(SLM)/probablities.py
@@ -0,0 +1,15 @@
+# Packages used.
+import random # For sampling from probability distributions.
+from collections import Counter, defaultdict # For counting n-grams.
+
+import textwrap # For automatically adding linebreaks to long texts.
+import pandas as pd # For constructing and visualizing tables.
+
+# Custom functions for providing feedback on your solutions.
+from ai_foundations.feedback.course_1 import ngrams
+
+africa_galore = pd.read_json(
+    "https://storage.googleapis.com/dm-educational/assets/ai_foundations/africa_galore.json"
+)
+dataset = africa_galore["description"]
+print(f"The dataset consists of {dataset.shape[0]} paragraphs.")
