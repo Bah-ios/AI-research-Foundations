c1de64748108814beb0664c84895d602525051ab
bah-ios <bahran.solomon@astu.edu.et>
2026-06-29 04:40:56 -0400

    n-gram counter added

--- 01 - Building my own Small language model(SLM)/probablities.py
+++ 01 - Building my own Small language model(SLM)/probablities.py
@@ -15,9 +15,9 @@ africa_galore = pd.read_json(
 dataset = africa_galore["description"]
 print(f"The dataset consists of {dataset.shape[0]} paragraphs.")
 
-for paragraph in dataset[:10]:
+""" for paragraph in dataset[:10]:
     formatted_paragraph = textwrap.fill(paragraph)
-    print(f"{formatted_paragraph}\n")
+    print(f"{formatted_paragraph}\n") """
 
 def tokenizer (text : str):
     tokenized = text.split(' ')
@@ -45,3 +45,12 @@ for paragraph in dataset:
 print(all_unigrams[:4])
 print(all_bigrams[:4])
 print(all_trigrams[:4])
+
+bigram_counter = Counter(all_bigrams)
+trigram_counter = Counter(all_trigrams)
+for bigram, count in bigram_counter.most_common(10):
+    print(f"  ({bigram}, {count})")
+
+print("\n\nMost common trigrams:")
+for trigram, count in trigram_counter.most_common(10):
+    print(f"  ({trigram}, {count})")
